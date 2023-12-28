477ff57b4f1045c530d0ddd06f251f3c0991a176
hhshiblu <hhshiblu5555@gmail.com>
2023-12-28 12:22:10 +0600

    jj

--- src/app/dynamic/[id]/page.jsx
+++ src/app/dynamic/[id]/page.jsx
@@ -1,15 +1,16 @@
 import connectToDB from "@/lib/connect";
+import { ObjectId } from "mongodb";
 import React from "react";
-const getcategory = async () => {
+const getsingle = async (id) => {
   const db = await connectToDB();
   const collection = db.collection("categori");
 
-  const category = await collection.find({}).toArray();
-  return category;
+  const singleOrder = await collection.findOne({ _id: new ObjectId(id) });
+  return singleOrder;
 };
-async function page({ id }) {
-  const data = await getcategory();
-  return <div>{id}</div>;
+async function page({ params }) {
+  const data = await getsingle(params.id);
+  return <div>{data.name}</div>;
 }
 
 export default page;
--- src/app/show-all/page.jsx
+++ src/app/show-all/page.jsx
@@ -1,5 +1,7 @@
 import connectToDB from "@/lib/connect";
+import Link from "next/link";
 import React from "react";
+
 const getcategory = async () => {
   const db = await connectToDB();
   const collection = db.collection("categori");
@@ -7,18 +9,20 @@ const getcategory = async () => {
   const category = await collection.find({}).toArray();
   return category;
 };
-async function page() {
+export const dynamic = "force-dynamic";
+
+async function Page() {
   const data = await getcategory();
   return (
     <div>
       {data &&
-        data?.map((option, i) => (
-          <option key={i} value={option._id}>
-            {option.name}
-          </option>
+        data.map((option, i) => (
+          <div key={i}>
+            <Link href={`/dynamic/${option._id}`}>{option.name}</Link>
+          </div>
         ))}
     </div>
   );
 }
 
-export default page;
+export default Page;
